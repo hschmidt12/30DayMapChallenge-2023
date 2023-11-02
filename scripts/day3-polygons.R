814260b8df4ebbfdfa8478598f8ab788f7248c52
hschmidt12 <37186998+hschmidt12@users.noreply.github.com>
2023-11-01 23:28:53 -0400

    days 3 and 4

--- scripts/day3-polygons.R
+++ scripts/day3-polygons.R
@@ -8,31 +8,71 @@ setwd("/Volumes/GoogleDrive/My Drive/SANLab/30DayMapChallenge-2023")
 # load packages
 library(tidyverse)
 library(sf)
-library(maps)
+library(MetBrewer)
+library(showtext)
 
-# get new zealand map
-nz <- map_data("nz") |>
-  select(lon = long, lat, group, id = region)
+# palette
+palette <- met.brewer("Hiroshige", n = 15)
+background <- "#36454f"
+  
+# fonts
+font_add_google("Macondo", "macondo") 
+showtext_auto()
+
+# get new zealand map ()
+nz <- read_sf("./data/statsnz-regional-tourism-organisation-areas-2023-clipped-SHP/regional-tourism-organisation-areas-2023-clipped.shp")
 
 # get coastal erosion data
-erosion <- st_read("./data/nzlri-erosion-type-and-severity.csv")
-# convert geometry to WGS84 coordinates (lat/lon)
-# centers <- as.data.frame(st_coordinates(erosion))
-# centers.subset <- centers[,1:2]
-# proj4 <- "+proj=tmerc"
-# lon.lat <- project(centers.subset, proj4, inverse = TRUE)
-# lon.lat <- data.frame(lon = lon.lat$x, lat = lon.lat$y)
+erosion <- read_sf("./data/lris-nzlri-erosion-type-and-severity-SHP/nzlri-erosion-type-and-severity.shp")
+# affected land areas only
+erosion <- subset(erosion, erosion$ERO1S == "1" | erosion$ERO1S == "2" | erosion$ERO1S == "3" | 
+                    erosion$ERO1S == "4" | erosion$ERO1S == "5")
 
+# create new erosion type labels; dominant erosion present
+erosion$label <- NA
+erosion$label[erosion$ERO1T == "Sh"] <- "sheet"
+erosion$label[erosion$ERO1T == "Sc"] <- "scree"
+erosion$label[erosion$ERO1T == "Ss"] <- "soil slip"
+erosion$label[erosion$ERO1T == "W"] <- "wind"
+erosion$label[erosion$ERO1T == "D"] <- "deposition"
+erosion$label[erosion$ERO1T == "Da"] <- "debris avalanche"
+erosion$label[erosion$ERO1T == "Ef"] <- "earthflow"
+erosion$label[erosion$ERO1T == "Es"] <- "earth slip"
+erosion$label[erosion$ERO1T == "G"] <- "gully"
+erosion$label[erosion$ERO1T == "T"] <- "tunnel gully"
+erosion$label[erosion$ERO1T == "Sb"] <- "streambank"
+erosion$label[erosion$ERO1T == "Su"] <- "slump"
+erosion$label[erosion$ERO1T == "Rs"] <- "riparian slip"
+erosion$label[erosion$ERO1T == "R"] <- "rill"
+erosion$label[erosion$ERO1T == "Rf"] <- "rockfall"
 
-# plot
-ggplot() +
-  geom_polygon(data = nz, aes(x = lon, y = lat, group = group)) +
-  #geom_polygon(data = lon.lat, aes(lon, y = lat), color = "blue") +
+p <- ggplot() +
+  geom_sf(data = nz, fill = "#DBE2E6", color = "#DBE2E6") +
+  geom_sf(data = erosion, aes(color = label, fill = label)) + 
+  scale_fill_manual(values = palette) +
+  scale_color_manual(values = palette) +
+  coord_sf() +
   theme_void() +
-  coord_map()
+  labs(title = "New Zealand Land Erosion") +
+  theme(# legend
+        legend.position = "bottom",
+        legend.margin = margin(b = 6, unit = "pt"),
+        legend.title = element_blank(),
+        legend.key.size = unit(0.5,"line"),
+        legend.text = element_text(size = 25),
+        # title
+        plot.title = element_text(hjust = 0.5, size = 50),
+        plot.margin = margin(t = 6, unit = "pt"),
+        # background
+        plot.background = element_rect(fill = background, color = background),
+        panel.background = element_rect(fill = background, color = background),
+        # text
+        text = element_text(color = "#DBE2E6", family = "macondo"))
+
 
 # save plot
-ggsave(filename = "./maps/day3_polygons.png",
+ggsave(plot = p, 
+       filename = "./maps/day3_polygons.png",
        width = 5,
        height = 5,
        units = "in",
--- scripts/day4-bad.R
+++ scripts/day4-bad.R
@@ -12,11 +12,16 @@ library(tidyverse)
 library(sf)
 library(raster)
 library(ggrepel)
+library(showtext)
 
 # palette
-background <- "#274c77"
-outline <- "#a3cef1"
-fill <- "#6096ba"
+fill <- "#274c77"
+background <- "#a3cef1"
+outline <- "#6096ba"
+  
+# fonts
+font_add_google("Germania One", "germania") 
+showtext_auto()
   
 # get germany map for background plot
 deutsch <- getData(country = "Germany", level = 1)
@@ -36,46 +41,74 @@ bad <- rbind(bad.city, Bad.city)
 bad$population <- as.numeric(bad$population)
 names(bad)[5] <- "Population"
 
-# add some city labels for plotting
+# add labels for notable spa town per state
 bad$label <- NA
-bad$label[bad$city == "Wiesbaden"] <- "Wiesbaden"
+# Baden-Württemberg
 bad$label[bad$city == "Baden-Baden"] <- "Baden-Baden"
+# Bavaria
 bad$label[bad$city == "Bad Kissingen"] <- "Bad Kissingen"
-bad$label[bad$city == "Bad Driburg"] <- "Bad Driburg"
-bad$label[bad$city == "Bad Dürkheim"] <- "Bad Dürkheim"
+# Berlin (nope, city)
+# Brandenburg
 bad$label[bad$city == "Bad Liebenwerda"] <- "Bad Liebenwerda"
+# Bremen (nope, city)
+# Hamburg (nope, city)
+# Hesse
+bad$label[bad$city == "Wiesbaden"] <- "Wiesbaden"
+# Lower Saxony
+bad$label[bad$city == "Bad Harzburg"] <- "Bad Harzburg"
+# Mecklenburg-Vorpommern
+bad$label[bad$city == "Bad Doberan"] <- "Bad Doberan"
+# North Rhine-Westphalia
+bad$label[bad$city == "Bad Driburg"] <- "Bad Driburg"
+# Rhineland-Palatinate
+bad$label[bad$city == "Bad Dürkheim"] <- "Bad Dürkheim"
+# Saarland (none in my list)
+# Saxony (none in my list)
+# Saxony-Anhalt (none in my list)
+# Schleswig-Holstein
+bad$label[bad$city == "Bad Schwartau"] <- "Bad Schwartau"
+# Thüringen
+bad$label[bad$city == "Bad Salzungen"] <- "Bad Salzungen"
+
+# add color variable for plotting
+bad$color <- NA
+bad$color[is.na(bad$label)] <- "0"
+bad$color[!is.na(bad$label)] <- "1"
+
+# add label position
+bad$position <- NA
+bad$position <- 16 - as.numeric(bad$lon)
+bad$position <- as.character(bad$position)
 
 # plot!
-ggplot() +
+p <- ggplot() +
   geom_polygon(data = deutsch,
                aes(x = long, y = lat, group = group),
                color = outline, fill = fill, linewidth = 0.25) +
   geom_point(data = bad, aes(x = as.numeric(lon), y = as.numeric(lat),
-                             size = scale(Population), color = rev(scale(Population)))) +
+                             color = color, size = color)) +
   geom_label_repel(data = bad, aes(x = as.numeric(lon), y = as.numeric(lat),
-                                   label = label),
-                   box.padding = 1.5, color = "#2E2E3A", fill = "#e7ecef") +
-  #scale_color_gradientn(colors = c("#e7ecef","#8b8c89","#2E2E3A")) +
-  scale_size_continuous(limits=c(0, 8), breaks=seq(0, 8, by=2)) +
+                                   label = label, color = color),
+                  box.padding = 0.1, fill = "#36454f",
+                  direction = "x", nudge_x = c(-40,20), point.padding = 0.25,
+                  size = 6, label.size = 0.25, fontface = "bold",
+                  segment.size = 0.25, segment.linetype = "dashed") +
+  scale_color_manual(values = c("#6096ba","#e7ecef")) +
+  scale_size_manual(values = c(1,3)) +
   theme_void() + 
   coord_map() +
-  theme(plot.background = element_rect(fill = background, color = background),
-        panel.background = element_rect(fill = background, color = background))
-
-
-
-  scale_color_gradientn(colors = c("#e7ecef","#8b8c89","#2E2E3A")) + 
-  #coord_map() +
-  #theme_void() +
+  labs(title = "Deutsche Badestädte",
+       caption = "Spa Cities in Germany") +
   theme(legend.position = "none",
+        plot.margin = margin(t = 3, b = 3, unit = "pt"),
         plot.background = element_rect(fill = background, color = background),
         panel.background = element_rect(fill = background, color = background),
-        text = element_text(color = outline),
-        legend.key.height = unit(0.5, "cm"), 
-        legend.key.width = unit(0.1, "cm"))
+        plot.title = element_text(family = "germania", size = 55, hjust = 0.5, color = "#36454f"),
+        plot.caption = element_text(family = "germania", size = 40, hjust = 0.5, color = "#36454f"))
 
 # save plot
 ggsave(filename = "./maps/day4_bad.png",
+       plot = p,
        width = 5,
        height = 5,
        units = "in",
